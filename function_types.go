4cfca1afecc5faaf8279cc83e7b36d6a4f5a4234
Johannes Holzfuß <DataWraith@web.de>
2015-02-15 19:30:00 +0100

    Room: Add InitializationFunc

--- function_types.go
+++ function_types.go
@@ -1,5 +1,10 @@
 package main
 
+// InitializationFunc specifies a function that is run once for a room to
+// initialize it. Since the initialization may depend on the current Gamestate,
+// it is called the first time a Room's Description method is called.
+type InitializationFunc func(g *Gamestate)
+
 // DescriptionFunc specifies a function that, given the current gamestate,
 // returns (part of) the description of a given room.
 type DescriptionFunc func(g *Gamestate) string
--- room.go
+++ room.go
@@ -1,20 +1,41 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Room represents a single location in the game. It has a description and
 // a set of actions that can be taken in the room. These actions act on the
 // global Gamestate.
 type Room struct {
-	DescriptionFuncs []DescriptionFunc
+	ID                 int
+	InitializationFunc InitializationFunc
+	DescriptionFuncs   []DescriptionFunc
 }
 
 // Description returns the description of a room, built from the current
 // Gamestate and the room's array of DescriptionFuncs.
-func (r Room) Description(g Gamestate) string {
+func (r Room) Description(g *Gamestate) string {
+	// Call the initialization function of the room if it wasn't called before
+	initFlag := fmt.Sprintf("room-%3d:initialized", r.ID)
+	if !g.HasFlag(initFlag) {
+		if r.InitializationFunc != nil {
+			r.InitializationFunc(g)
+		}
+
+		g.SetFlag(initFlag)
+	}
+
+	// Build the room description
 	result := ""
-	for _, p := range r.DescriptionFuncs {
-		result += p(g)
+	for _, dfunc := range r.DescriptionFuncs {
+		desc := dfunc(g)
+		if desc == "" {
+			continue
+		}
+
+		result += desc
 		result += "\n\n"
 	}
-	result = result[0 : len(result)-2]
 	return result
 }
